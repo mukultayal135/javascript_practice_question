31e8a08298521e5560bf2e6d2499740350cfc735
Mukul Tayal <mukultayal135@gmail.com>
2023-01-15 19:29:42 +0530

    chore: test largest even number

--- testing/q1.test.js
+++ testing/q1.test.js
@@ -1,4 +1,4 @@
-const {largest_even,largest_even_arrow}= require('./q1');
+const {largest_even,largest_even_arrow}= require('../q1');
 test('Find largest even number',() =>
 {
     expect(largest_even([2,6,8])).toBe(8)
