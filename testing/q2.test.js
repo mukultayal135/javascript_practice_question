ea39da9c8c146f9ed6c8f4146acccb3ee5a7a9e2
Mukul Tayal <mukultayal135@gmail.com>
2023-01-15 19:00:08 +0530

    fix: moving testing files to folder

---
+++ testing/q2.test.js
@@ -0,0 +1,7 @@
+const {same_digits,same_digits_arrow} = require('./q2');
+test('If all digits are same',() => {
+    expect(same_digits(223)).toBe(false);
+});
+test('If all digits are same',() => {
+    expect(same_digits_arrow(4444)).toBe(true);
+});
