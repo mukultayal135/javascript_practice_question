47e7fb9332ee5f7fa5980b77f79167b8b2800054
Mukul Tayal <mukultayal135@gmail.com>
2023-01-15 19:32:36 +0530

    chore: test change of capitalisation

---
+++ testing/q3.test.js
@@ -0,0 +1,12 @@
+const {change_capitalisation,change_capitalisation_arrow}=require('../q3.js');
+
+test('test change capitalisation',()=>
+{
+    expect(change_capitalisation("Mukul Tayal")).toBe("mUKUL tAYAL");
+
+});
+test('test change capitalisation arrow',()=>
+{
+    expect(change_capitalisation_arrow("Mukul Tayal")).toBe("mUKUL tAYAL");
+
+});
