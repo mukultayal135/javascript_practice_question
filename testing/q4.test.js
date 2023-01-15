136efd33b51bd62c912a1ab44a372dc9ee82ad8a
Mukul Tayal <mukultayal135@gmail.com>
2023-01-15 19:44:44 +0530

    chore: test longest string from array

---
+++ testing/q4.test.js
@@ -0,0 +1,10 @@
+const { default: expect } = require('expect');
+const {get_longest_string,get_longest_string_arrow}=require('../q4');
+test('test to find longest string',()=>
+{
+    expect(get_longest_string(["we","are","doing","code","academy"])).toBe("academy");
+});
+test('test to find longest string',()=>
+{
+    expect(get_longest_string_arrow(["we","are","doing","code","academy"])).toBe("academy");
+});
