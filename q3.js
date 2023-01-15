895ec46343e0e20e59df577e6d09945d62696460
Mukul Tayal <mukultayal135@gmail.com>
2023-01-15 13:01:22 +0530

    chore:change capitalisation of string

---
+++ q3.js
@@ -0,0 +1,35 @@
+function change_capitalisation(st)
+{
+    let ans="";
+    for(let i=0; i<st.length; i++)
+    {
+        if(st[i] != st[i].toUpperCase())
+        {
+            ans+=st[i].toUpperCase();
+        }
+        else{
+            ans+=st[i].toLowerCase();  
+        }
+    }
+    return ans;
+}
+const change_capitalisation_arrow = st =>
+{
+    let ans="";
+    for(let i=0; i<st.length; i++)
+    {
+        if(st[i] != st[i].toUpperCase())
+        {
+            ans+=st[i].toUpperCase();
+        }
+        else{
+            ans+=st[i].toLowerCase();
+            
+        }
+       
+    }
+    return ans;
+}
+
+console.log(change_capitalisation("Mukul Tayal"));
+console.log(change_capitalisation_arrow("Mukul Tayal"));
