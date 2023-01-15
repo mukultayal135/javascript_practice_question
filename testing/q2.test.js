bf4bf565b67decee1eb2588e26219c348f3748f6
Mukul Tayal <mukultayal135@gmail.com>
2023-01-15 19:30:45 +0530

    chore: test all digits are same

--- testing/q2.test.js
+++ testing/q2.test.js
@@ -1,4 +1,4 @@
-const {same_digits,same_digits_arrow} = require('./q2');
+const {same_digits,same_digits_arrow} = require('../q2');
 test('If all digits are same',() => {
     expect(same_digits(223)).toBe(false);
 });
