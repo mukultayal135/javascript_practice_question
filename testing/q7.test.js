28ab59ad29d5986db0abcd3fe25c597cee6f2795
Mukul Tayal <mukultayal135@gmail.com>
2023-01-15 23:14:25 +0530

    chore: test concatenating without first letter

---
+++ testing/q7.test.js
@@ -0,0 +1,9 @@
+const {concat_without_first,concat_without_first_arrow}=require('../q7.js');
+test('test concatenating without first letter',()=>
+{
+    expect(concat_without_first("code","academy")).toBe("odecademy");
+})
+test('test if string ends with "Script"',()=>
+{
+    expect(concat_without_first("code","academy")).toBe("odecademy");
+})
