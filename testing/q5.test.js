7c40f7e9afc3b66b2a5a4c3dbcb9ad947f8c4699
Mukul Tayal <mukultayal135@gmail.com>
2023-01-15 22:40:50 +0530

    chore: test if year is leap year or not

---
+++ testing/q5.test.js
@@ -0,0 +1,9 @@
+const {leap_year,leap_year_arrow}=require('../q5.js');
+test('test to check if year is leap or not',()=>
+{
+    expect(leap_year(2000)).toBe(true);
+})
+test('test to check if year is leap or not',()=>
+{
+    expect(leap_year_arrow(2000)).toBe(true);
+})
